a7ad8fc9427862a85277484d3d52cbfcc15aac23
Paritosh Sabade <paritoshsabade@Paritoshs-MacBook-Air.local>
2023-07-19 16:40:23 -0400

    Updates

---
+++ precache-manifest.b09da66b61a9ec261db72cb3391da290.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eaea001400bb7930f198",
+    "url": "/portfolio/static/css/main.3ca47667.chunk.css"
+  },
+  {
+    "revision": "eaea001400bb7930f198",
+    "url": "/portfolio/static/js/main.eaea0014.chunk.js"
+  },
+  {
+    "revision": "98962da333049074e89d",
+    "url": "/portfolio/static/js/1.98962da3.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "49b78dacd952574e6a15646f2088dba2",
+    "url": "/portfolio/index.html"
+  }
+];
